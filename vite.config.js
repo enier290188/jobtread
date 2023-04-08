02320cec67f96d702e6121a1a1091dc02d71a866
Enier Ramos García <enier290188@gmail.com>
2023-04-08 18:10:16 -0500

    Adapted Vite to work in Amplify

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // Fix AWS Amplify issue where incorrect entry point is used
-    // Error: 'request' is not exported by __vite-browser-external, imported by node_modules/@aws-sdk/credential-provider-imds/dist/es/remoteProvider/httpRequest.js
-    // Github Issue: https://github.com/aws/aws-sdk-js/issues/3673
+    define: {
+        // AWS Amplify needs this to work
+        // By default, Vite doesn't include shims for NodeJS necessary for segment analytics lib to work
+        // GitHub Issue: https://github.com/aws/aws-sdk-js/issues/3673
+        global: {}
+    },
     resolve: {
         alias: {
+            // AWS Amplify needs this to work
+            // Error: "request" is not exported by "__vite-browser-external", imported by "node_modules/@aws-sdk/credential-provider-imds/dist/es/remoteProvider/httpRequest.js"
+            // GitHub Issue: https://github.com/aws/aws-sdk-js/issues/3673
             './runtimeConfig': './runtimeConfig.browser'
         }
     }
