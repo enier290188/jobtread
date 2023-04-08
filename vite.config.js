344ffd1f198cb4e220a7ee05757dc8229175ae18
Enier Ramos García <enier290188@gmail.com>
2023-04-08 17:16:18 -0500

    Adapted Vite to work in Amplify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {outDir: 'build'}
+    // Fix AWS Amplify issue where incorrect entry point is used
+    // Error: 'request' is not exported by __vite-browser-external, imported by node_modules/@aws-sdk/credential-provider-imds/dist/es/remoteProvider/httpRequest.js
+    // Github Issue: https://github.com/aws/aws-sdk-js/issues/3673
+    resolve: {
+        alias: {
+            './runtimeConfig': './runtimeConfig.browser'
+        }
+    }
 })
