3ef44502d28f86bb545649dd80c1a630e49e3b3f
Enier Ramos García <enier290188@gmail.com>
2023-04-13 16:23:25 -0500

    Updated theme

--- src/app/layout/footer/Footer.jsx
+++ src/app/layout/footer/Footer.jsx
@@ -20,7 +20,7 @@ export const Footer = () => {
         <Box component={'footer'} m={0} pt={0} pr={2} pb={0} pl={2} sx={sxFooter}>
             <Container component={'div'}>
                 <Box component={'div'} sx={sxContainer}>
-                    <Typography component={'p'} variant={'body2'} textAlign={'center'}>{date.getFullYear()}, Celsitud LLC. All rights reserved.</Typography>
+                    <Typography component={'p'} variant={'caption'} textAlign={'center'}>{date.getFullYear()}, Celsitud LLC. All rights reserved.</Typography>
                 </Box>
             </Container>
         </Box>
--- src/app/layout/header/Header.jsx
+++ src/app/layout/header/Header.jsx
@@ -14,7 +14,7 @@ const LinkCustom = ({children, to, ...props}) => {
     )
 }
 
-const Brand = () => {
+const MobileBrand = () => {
     const sxContainer = {
         display: 'flex',
         flexDirection: 'row',
@@ -26,14 +26,145 @@ const Brand = () => {
         color: theme => theme.palette.common.white
     }
     const sxAvatar = {
-        width: theme => theme.spacing(10),
-        height: theme => theme.spacing(10)
+        width: theme => theme.spacing(11),
+        height: theme => theme.spacing(11)
+    }
+    const sxTypography = {
+        display: {xs: 'none', sm: 'block'}
     }
 
     return (
         <Box component={NavLink} to={SettingRoute.app.TO} sx={sxContainer}>
             <Avatar component={'span'} variant={'square'} src={AssetSvgBrand} sx={sxAvatar}/>
-            <Typography component={'span'} variant={'h4'}>JobTread</Typography>
+            <Typography component={'span'} variant={'h4'} sx={sxTypography}>JobTread</Typography>
+        </Box>
+    )
+}
+
+const MobileContent = () => {
+    const sxContent = {
+        display: {xs: 'flex', sm: 'none'},
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        alignContent: 'flex-start',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    }
+
+    return (
+        <Box component={'div'} sx={sxContent}>
+            ...
+        </Box>
+    )
+}
+
+const DesktopContentLeftBrand = () => {
+    const sxContainer = {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        alignContent: 'flex-start',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        textDecoration: 'none',
+        color: theme => theme.palette.common.white
+    }
+    const sxAvatar = {
+        width: theme => theme.spacing(11),
+        height: theme => theme.spacing(11)
+    }
+    const sxTypography = {}
+
+    return (
+        <Link component={NavLink} to={SettingRoute.app.TO} sx={sxContainer}>
+            <Avatar component={'span'} variant={'square'} src={AssetSvgBrand} sx={sxAvatar}/>
+            <Typography component={'span'} variant={'h4'} sx={sxTypography}>JobTread</Typography>
+        </Link>
+    )
+}
+
+const DesktopContentLeftLink = ({children, to, ...props}) => {
+    const resolvedPath = useResolvedPath(to)
+    const match = useMatch({path: resolvedPath.pathname, end: false})
+
+    return (
+        <Link component={NavLink} to={to} sx={{textDecoration: match ? 'underline' : 'none', color: theme => theme.palette.common.white}} {...props}>
+            {children}
+        </Link>
+    )
+}
+
+const DesktopContentLeft = () => {
+    const sxContainer = {
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        alignContent: 'flex-start',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+
+        border: '1px solid white'
+    }
+
+    return (
+        <Box component={'div'} sx={sxContainer}>
+            <DesktopContentLeftBrand/>
+            <Box component={'div'} mx={2}/>
+            <Box component={'div'} p={1}>
+                <LinkCustom to={`${SettingRoute.app.dashboard.TO}`}>Dashboard</LinkCustom>
+            </Box>
+            <Box component={'div'} p={1}>
+                <LinkCustom to={`${SettingRoute.app.task.TO}`}>Tasks</LinkCustom>
+            </Box>
+            <Box component={'div'} p={1}>
+                <LinkCustom to={`${SettingRoute.app.error.TO}`}>Error</LinkCustom>
+            </Box>
+        </Box>
+    )
+}
+
+const DesktopContentRight = () => {
+    const sxContainer = {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        alignContent: 'flex-start',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+
+        border: '1px solid white'
+    }
+
+    return (
+        <Box component={'div'} sx={sxContainer}>
+            <Box component={'div'} p={1}>
+                <LinkCustom to={`${SettingRoute.app.dashboard.TO}`}>Dashboard</LinkCustom>
+            </Box>
+            <Box component={'div'} p={1}>
+                <LinkCustom to={`${SettingRoute.app.task.TO}`}>Tasks</LinkCustom>
+            </Box>
+            <Box component={'div'} p={1}>
+                <LinkCustom to={`${SettingRoute.app.error.TO}`}>Error</LinkCustom>
+            </Box>
+        </Box>
+    )
+}
+
+const DesktopContent = () => {
+    const sxContainer = {
+        display: {xs: 'none', sm: 'flex'},
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        alignContent: 'flex-start',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    }
+
+    return (
+        <Box component={'div'} sx={sxContainer}>
+            <DesktopContentLeft/>
+            <DesktopContentRight/>
         </Box>
     )
 }
@@ -51,62 +182,13 @@ export const Header = () => {
         justifyContent: 'flex-start',
         alignItems: 'stretch'
     }
-    const sxContent = {
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'nowrap',
-        alignContent: 'flex-start',
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-    }
-    const sxContentLeft = {
-        flexGrow: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'nowrap',
-        alignContent: 'flex-start',
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-    }
-    const sxContentRight = {
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'nowrap',
-        alignContent: 'flex-start',
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-    }
 
     return (
         <Box component={'header'} m={0} pt={1} pr={2} pb={1} pl={2} sx={sxHeader}>
             <Container component={'div'}>
                 <Box component={'div'} sx={sxContainer}>
-                    <Box component={'div'} sx={sxContent}>
-                        <Box component={'div'} sx={sxContentLeft}>
-                            <Brand/>
-                            <Box component={'div'} mx={2}/>
-                            <Box component={'div'} p={1}>
-                                <LinkCustom to={`${SettingRoute.app.dashboard.TO}`}>Dashboard</LinkCustom>
-                            </Box>
-                            <Box component={'div'} p={1}>
-                                <LinkCustom to={`${SettingRoute.app.task.TO}`}>Task</LinkCustom>
-                            </Box>
-                            <Box component={'div'} p={1}>
-                                <LinkCustom to={`${SettingRoute.app.error.TO}`}>Error</LinkCustom>
-                            </Box>
-                        </Box>
-                        <Box component={'div'} sx={sxContentRight}>
-                            <Box component={'div'} p={1}>
-                                <LinkCustom to={`${SettingRoute.app.dashboard.TO}`}>Dashboard</LinkCustom>
-                            </Box>
-                            <Box component={'div'} p={1}>
-                                <LinkCustom to={`${SettingRoute.app.task.TO}`}>Task</LinkCustom>
-                            </Box>
-                            <Box component={'div'} p={1}>
-                                <LinkCustom to={`${SettingRoute.app.error.TO}`}>Error</LinkCustom>
-                            </Box>
-                        </Box>
-                    </Box>
+                    <MobileContent/>
+                    <DesktopContent/>
                 </Box>
             </Container>
         </Box>
