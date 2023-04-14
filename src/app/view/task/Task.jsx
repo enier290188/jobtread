e172e1397b963513b8b44bf9f8eea34d3c42909c
Enier Ramos García <enier290188@gmail.com>
2023-04-14 13:29:17 -0500

    Updated View Task

--- src/app/view/task/Task.jsx
+++ src/app/view/task/Task.jsx
@@ -1,40 +1,36 @@
-import {Box, Divider, Link, Typography} from '@mui/material'
-import {Navigate, NavLink, Outlet, Route, Routes, useMatch, useResolvedPath} from 'react-router-dom'
-import {ComponentNavigateToAppErrorNotFound} from '../../component'
+import {Task as IconTask} from '@mui/icons-material'
+import {Divider} from '@mui/material'
+import {Outlet, Route, Routes} from 'react-router-dom'
+import {ComponentContainer, ComponentContainerContent, ComponentContainerHead, ComponentContainerHeadTitle, ComponentContainerHeadTitleIcon, ComponentContainerHeadTitleTypography, ComponentNavigateToAppErrorNotFound} from '../../component'
 import {SettingRoute} from '../../setting'
 import {RouteContent001} from './content001'
 import {RouteContent002} from './content002'
 import {RouteContent003} from './content003'
 
-const LinkCustom = ({children, to, ...props}) => {
-    const resolvedPath = useResolvedPath(to)
-    const match = useMatch({path: resolvedPath.pathname, end: false})
-
+const Layout = () => {
     return (
-        <Link component={NavLink} to={to} sx={{textDecoration: match ? 'underline' : 'none'}} {...props}>
-            {children}
-        </Link>
+        <Outlet/>
     )
 }
 
-const Layout = () => {
+const View = () => {
     return (
-        <Box component={'div'}>
-            <Typography component={'p'} variant={'body1'}>---Layout Main Task---</Typography>
-            <Box component={'div'} display={'flex'}>
-                <Box component={'div'} p={1}>
-                    <LinkCustom to={`${SettingRoute.app.task.content001.TO}`}>Content001</LinkCustom>
-                </Box>
-                <Box component={'div'} p={1}>
-                    <LinkCustom to={`${SettingRoute.app.task.content002.TO}`}>Content002</LinkCustom>
-                </Box>
-                <Box component={'div'} p={1}>
-                    <LinkCustom to={`${SettingRoute.app.task.content003.TO}`}>Content003</LinkCustom>
-                </Box>
-            </Box>
-            <Divider component={'div'}/>
-            <Outlet/>
-        </Box>
+        <ComponentContainer>
+            <ComponentContainerHead>
+                <ComponentContainerHeadTitle>
+                    <ComponentContainerHeadTitleIcon>
+                        <IconTask/>
+                    </ComponentContainerHeadTitleIcon>
+                    <ComponentContainerHeadTitleTypography>
+                        {'Tasks'}
+                    </ComponentContainerHeadTitleTypography>
+                </ComponentContainerHeadTitle>
+            </ComponentContainerHead>
+            <Divider/>
+            <ComponentContainerContent>
+                ...
+            </ComponentContainerContent>
+        </ComponentContainer>
     )
 }
 
@@ -42,7 +38,7 @@ export const Task = () => {
     return (
         <Routes>
             <Route path={``} element={<Layout/>}>
-                <Route index element={<Navigate to={`${SettingRoute.app.task.content002.TO}`}/>}/>
+                <Route index element={<View/>}/>
                 <Route path={`${SettingRoute.app.task.content001.PATH}*`} element={<RouteContent001/>}/>
                 <Route path={`${SettingRoute.app.task.content002.PATH}*`} element={<RouteContent002/>}/>
                 <Route path={`${SettingRoute.app.task.content003.PATH}*`} element={<RouteContent003/>}/>
