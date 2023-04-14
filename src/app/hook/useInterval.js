4e999ec0af1608da5ec9b33f3b8ec2e92febb43c
Enier Ramos García <enier290188@gmail.com>
2023-04-14 18:54:19 -0500

    Updated useInterval hook

--- src/app/hook/useInterval.js
+++ src/app/hook/useInterval.js
@@ -5,9 +5,23 @@ const getIntervalInitial = () => Date.now()
 export const useInterval = (timeout = 1000) => {
     const [intervalState, setIntervalState] = useState(getIntervalInitial)
     const intervalRef = useRef(null)
+
+    const startInterval = () => {
+        if (intervalRef.current) {
+            stopInterval()
+        }
+        intervalRef.current = setInterval(() => setIntervalState(Date.now()), timeout)
+    }
+
+    const stopInterval = () => {
+        clearInterval(intervalRef.current)
+    }
+
     useEffect(() => {
         intervalRef.current = setInterval(() => setIntervalState(Date.now()), timeout)
+
         return () => clearInterval(intervalRef.current) // Cleanup
     }, [])
-    return [intervalState]
+
+    return [intervalState, startInterval, stopInterval]
 }
